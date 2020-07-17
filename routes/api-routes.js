cc01874ae8d92636acf4642c7e936cfbd39aaf07
hilow2341 <60238749+hilow2341@users.noreply.github.com>
2020-07-17 14:22:20 -0400

    Included ability to add an exercise

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,12 @@
 const db = require("../models");
 
-
 module.exports = function (app) {
 
+    //Get Workouts
     app.get("/api/workouts", (req, res) => {
 
         db.Workout.find({}).then(dbWorkout => {
-            // console.log("ALL WORKOUTS");
-            // console.log(dbWorkout);
+
             dbWorkout.forEach(workout => {
                 var total = 0;
                 workout.exercises.forEach(e => {
@@ -21,6 +20,23 @@ module.exports = function (app) {
         }).catch(err => {
             res.json(err);
         });
+    });
+
+    // add exercise
+    app.put("/api/workouts/:id", (req, res) => {
+
+        db.Workout.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                $inc: { totalDuration: req.body.duration },
+                $push: { exercises: req.body }
+            },
+            { new: true }).then(dbWorkout => {
+                res.json(dbWorkout);
+            }).catch(err => {
+                res.json(err);
+            });
+
     });
 
 }
