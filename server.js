41783781ffb6b47e987c598311ce49b286f6b373
hilow2341 <60238749+hilow2341@users.noreply.github.com>
2020-07-17 14:34:49 -0400

    Set up server and use of express

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
